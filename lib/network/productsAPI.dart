4cd4b2a67d47f9115288e552b6dc4524eafe2bbf
Aliaa Asaad <aliaaasaad35@gmail.com>
2022-11-10 23:25:48 +0200

    aliaa it is all done

--- lib/network/productsAPI.dart
+++ lib/network/productsAPI.dart
@@ -4,19 +4,6 @@ import 'package:http/http.dart' as http;
 import 'package:intermediate/model/products.dart';
 
 class ProductsApi {
-  Future<ProductsData>? getProductData() async {
-    final http.Response response =
-        await http.get(Uri.parse("https://fakestoreapi.com/products"));
-    if (response.statusCode <= 299 && response.statusCode >= 200) {
-      List<dynamic> body = jsonDecode(response.body);
-      ProductsData products = ProductsData.fromMap(body);
-      print(body);
-      return products;
-    } else {
-      throw ('failed' + response.body);
-    }
-  }
-
   Future<Products>? getSingleProduct(String id) async {
     Products().id = int.parse(id);
     final http.Response response =
